d648aa35cba81e2bdbf752bd34c3fd2791379b16
unknown <Nehemie@BON2020>
2022-12-09 14:59:27 +0000

    git commit

--- trackitbackend/src/main/java/com/example/trackitbackend/entities/User.java
+++ trackitbackend/src/main/java/com/example/trackitbackend/entities/User.java
@@ -1,6 +1,17 @@
 package com.example.trackitbackend.entities;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	String id;
