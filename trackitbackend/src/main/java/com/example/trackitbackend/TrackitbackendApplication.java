09dc67fc687208eb48a820abc3595b2016dbf0d1
Nehemie <Nehemie@BON2020>
2022-12-09 14:06:55 +0000

    git commit

---
+++ trackitbackend/src/main/java/com/example/trackitbackend/TrackitbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackitbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackitbackendApplication.class, args);
+	}
+
+}
---
+++ trackitbackend/src/test/java/com/example/trackitbackend/TrackitbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackitbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
